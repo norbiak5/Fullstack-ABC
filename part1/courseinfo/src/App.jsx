5cdadf76c9aac11b31e0f0394042829a6dce8744
NorbertRogulski <norbiak5@gmail.com>
2023-11-27 12:13:19 +0100

    niewiem

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+ 
+// Header component
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  );
+};
+ 
+// Part component
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+ 
+// Content component
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+ 
+// Total component
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+ 
+  return (
+    <p><strong>Total exercises: {totalExercises}</strong></p>
+  );
+};
+ 
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+ 
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+ 
+export default App;
